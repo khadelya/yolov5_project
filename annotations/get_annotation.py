411a0583a5e629ff28ef0288d2fe54ee129c0056
kh.adelya <AKhusnullina@croc.ru>
2022-06-06 13:31:54 +0300

    get_annotation.py formatting upd

--- annotations/get_annotation.py
+++ annotations/get_annotation.py
@@ -4,32 +4,32 @@ import json
 import os
 
 
-def JsonToNewCoord(TifFileName, JsonFileName):
+def json_to_new_coord(tif_file_name, json_file_name):
     # projwin_srs = "EPSG:2326"
-    ds=gdal.Open(f'{TifFileName}.tif')
+    ds=gdal.Open(f'{tif_file_name}.tif')
     prj=ds.GetProjection()
     srs=osr.SpatialReference(wkt=prj)
     src_epsg = int(srs.GetAuthorityCode(None)) # src - .tif file
-    json_epsg = 4326 # EPSG WGS 84 of json file
-    transformer = Transformer.from_crs(json_epsg, src_epsg)
+    JSON_EPSG = 4326 # EPSG WGS 84 of json file
+    transformer = Transformer.from_crs(JSON_EPSG, src_epsg)
 
-    with open(f"{JsonFileName}.json", "r+") as f:
+    with open(f"{json_file_name}.json", "r+") as f:
         train = json.load(f)
         features = train["features"]
         for feature in features:
             coordinates = feature["geometry"]["coordinates"][0]
             for coordinate in coordinates:
-                lat, long = coordinate
-                coordinate[1], coordinate[0] = transformer.transform(long, lat)
+                latitude, longitude = coordinate
+                coordinate[1], coordinate[0] = transformer.transform(longitude, latitude)
 
-    with open(f'{JsonFileName}_new_coord.json', 'w', encoding='utf-8') as f:
+    with open(f'{json_file_name}_new_coord.json', 'w', encoding='utf-8') as f:
         json.dump(train, f, ensure_ascii=False, indent=2)
     return
 
 
-# Json file should be in the new coordinates f'{JsonFileName}_new_coord.json'
-def CropTif(TifFileName, JsonFileName):
-    with open(f"{JsonFileName}_new_coord.json", "r") as f:
+# Json file should be in the new coordinates f'{json_file_name}_new_coord.json'
+def crop_tif(tif_file_name, json_file_name):
+    with open(f"{json_file_name}_new_coord.json", "r") as f:
         train = json.load(f)
         features = train["features"]
         for i in range(len(features)):
@@ -38,10 +38,10 @@ def CropTif(TifFileName, JsonFileName):
             xmax = train["features"][i]["geometry"]["coordinates"][0][2][0]
             ymax = train["features"][i]["geometry"]["coordinates"][0][1][1]
             window = (xmin, ymax, xmax, ymin)
-            gdal.Translate(f'../data/images/train/output_crop_raster_{i}.tif', f'{TifFileName}.tif', projWin = window)
+            gdal.Translate(f'../data/images/train/output_crop_raster_{i}.tif', f'{tif_file_name}.tif', projWin = window)
     return
 
-def GetBoxCoordinates(feature):
+def get_box_coordinates(feature):
     x_list = []
     y_list = []
     coordinates = feature["geometry"]["coordinates"][0]
@@ -59,47 +59,47 @@ def GetBoxCoordinates(feature):
 
 # features[0]["geometry"]["coordinates"][0][0][0]
 
-def GetPixelValues(box_coordinates, UpperLeft, pixelSize):
-    xmin_px = round((box_coordinates[0] - UpperLeft[0]) / pixelSize[0]) # round((x_coord - UpperLeftX) / pixelSizeX)
-    ymin_px = round((UpperLeft[1] - box_coordinates[1]) / pixelSize[1]) # round((UpperLeftY - y_coord) / pixelSizeY)
+def get_pixel_values(box_coordinates, UpperLeft, pixelSize):
+    xmin_px = round((box_coordinates[0] - UpperLeft[0]) / pixelSize[0]) # round((x_coord - upper_left_x) / px_size_x)
+    ymin_px = round((UpperLeft[1] - box_coordinates[1]) / pixelSize[1]) # round((upper_left_y - y_coord) / px_size_y)
     xmax_px = round((box_coordinates[2] - UpperLeft[0]) / pixelSize[0])
     ymax_px = round((UpperLeft[1] - box_coordinates[3]) / pixelSize[1])
     return [xmin_px, ymin_px, xmax_px, ymax_px]
 
-def Convert(size, box):
-    dw = 1. / size[0]
-    dh = 1. / size[1]
-    x = (box[0] + box[2]) * dw / 2
-    y = (box[1] + box[3]) * dh / 2
-    w = (box[2] - box[0]) * dw
-    h = (box[3] - box[1]) * dh
-    return (x, y, w, h)
+def convert(size, box):
+    width_norm = 1. / size[0]
+    height_norm = 1. / size[1]
+    x_center = (box[0] + box[2]) * width_norm / 2
+    y_center = (box[1] + box[3]) * height_norm / 2
+    width = (box[2] - box[0]) * width_norm
+    height = (box[3] - box[1]) * height_norm
+    return (x_center, y_center, width, height)
 
-def ConvertAnnotation(TifFileName, JsonFileNameTrain, JsonFileNameManual):
+def convert_annotation(tif_file_name, json_file_name_train, json_file_name_manual):
     # crop tif file
-    JsonToNewCoord(TifFileName, JsonFileNameTrain)
-    with open(f"{JsonFileNameTrain}_new_coord.json", "r") as f:
+    json_to_new_coord(tif_file_name, json_file_name_train)
+    with open(f"{json_file_name_train}_new_coord.json", "r") as f:
         train = json.load(f)
         features_train = train["features"]
     num_of_files = len(features_train)
-    CropTif(TifFileName, JsonFileNameTrain)
+    crop_tif(tif_file_name, json_file_name_train)
 
-    JsonToNewCoord(TifFileName,JsonFileNameManual)
+    json_to_new_coord(tif_file_name,json_file_name_manual)
 
-    with open(f"{JsonFileNameManual}_new_coord.json", "r") as f:
+    with open(f"{json_file_name_manual}_new_coord.json", "r") as f:
         data = json.load(f)
         features = data["features"]
     for i in range(num_of_files):
         raster = gdal.Open(f'../data/images/train/output_crop_raster_{i}.tif')
         gt = raster.GetGeoTransform()
-        pixelSizeX = gt[1]
-        pixelSizeY =-gt[5]
+        px_size_x = gt[1]
+        px_size_y =-gt[5]
         width = raster.RasterXSize
         height = raster.RasterYSize
-        UpperLeftX = gt[0] # xmin
-        UpperLeftY = gt[3] # ymax
-        LowerRightX = gt[0] + width*gt[1] + height*gt[2] # xmax
-        LowerRightY = gt[3] + width*gt[4] + height*gt[5] # ymin
+        upper_left_x = gt[0] # xmin
+        upper_left_y = gt[3] # ymax
+        lower_right_x = gt[0] + width*gt[1] + height*gt[2] # xmax
+        lower_right_y = gt[3] + width*gt[4] + height*gt[5] # ymin
 
         outfile = open(f'../data/labels/train/output_crop_raster_{i}.txt', 'a+')
 
@@ -107,17 +107,17 @@ def ConvertAnnotation(TifFileName, JsonFileNameTrain, JsonFileNameManual):
             sum = 0
             coordinates = feature["geometry"]["coordinates"][0]
             for i in range(len(coordinates)):
-                if (UpperLeftX <= coordinates[i][0] <= LowerRightX) & (LowerRightY <= coordinates[i][1] <= UpperLeftY):
+                if (upper_left_x<=coordinates[i][0]<=lower_right_x) & (lower_right_y<=coordinates[i][1]<=upper_left_y):
                     sum += 1
 
             if sum == len(coordinates):
-                box_coordinates = GetBoxCoordinates(feature)
-                cls_id = 0
-                box = GetPixelValues(box_coordinates, (UpperLeftX, UpperLeftY), (pixelSizeX, pixelSizeY))
-                bbox = Convert((width, height), box)
-                outfile.write(str(cls_id) + " " + " ".join([str(a) for a in bbox]) + '\n')
+                box_coordinates = get_box_coordinates(feature)
+                class_id = 0
+                box = get_pixel_values(box_coordinates, (upper_left_x, upper_left_y), (px_size_x, px_size_y))
+                bounding_box = convert((width, height), box)
+                outfile.write(str(class_id) + " " + " ".join([str(bbox_coordinate) for bbox_coordinate in bounding_box]) + '\n')
 
         outfile.close()
 
 if __name__ == '__main__':
-    ConvertAnnotation('orthophoto', 'boat_ANNOTATIONS_TRAIN', 'boat_ANNOTATIONS_MANUAL')
+    convert_annotation('orthophoto', 'boat_ANNOTATIONS_TRAIN', 'boat_ANNOTATIONS_MANUAL')
